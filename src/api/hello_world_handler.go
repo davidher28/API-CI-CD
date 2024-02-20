59c08684f85dd60582463c7838f7e377b742d677
Claudia Sanchez <claudia.sanchez@mango.com>
2024-02-20 21:36:18 +0100

    fix blank space

--- src/api/hello_world_handler.go
+++ src/api/hello_world_handler.go
@@ -14,6 +14,5 @@ func HelloWorldHandler(w http.ResponseWriter, _ *http.Request) {
 	jsonData := EncodeJSONResponse(response)
 
 	WriteJSONResponse(w, http.StatusOK, jsonData)
-
 	fmt.Printf("hello world was executed")
 }
